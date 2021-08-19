d5a84c0625b47722ac08a8cf95d7a9272da67384
Wijayaac <wijayaac25@gmail.com>
2021-08-19 10:46:54 +0800

    🔥 unhide connections files

---
+++ utils/connection.php
@@ -0,0 +1,16 @@
+<?php
+function connectDB()
+{
+    $serverName = 'localhost';
+    $databaseName = 'db_jwd';
+    $username = 'phpmyadmin';
+    $password = 'ac';
+
+    // Create connection into database
+    $connection = mysqli_connect($serverName, $username, $password, $databaseName);
+
+    if (mysqli_connect_errno()) {
+        die('Connection failed : ' . mysqli_connect_error());
+    }
+    return $connection;
+}
